c91d17b6a28fa90fb8d0a3513cb30fce326d24e0
William Spies <spieswl18@u.northwestern.edu>
2018-05-01 12:24:26 -0500

    HW8 (HW1 redux) completed and tested on the PIC32.

---
+++ HW8/firmware/HW8.X/build/default/production/_ext/1360937237/main.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1360937237/main.o:  \
+../src/main.c  \
+ ../../../../../../../../../../opt/microchip/harmony/v2_05/framework/system/common/sys_module.h  \
+ ../../../../../../../../../../opt/microchip/harmony/v2_05/framework/system/common/sys_common.h 
