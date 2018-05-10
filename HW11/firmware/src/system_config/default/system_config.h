b35f20a13833d6978c4b57fe81b2ad7a5a0cc19d
William Spies <spieswl18@u.northwestern.edu>
2018-05-10 17:26:31 -0500

    HW11 complete; mouse works, tested it just a little bit ago. Needed to remove the (-1) being multiplied by the values for it to not have "inverted X, inverted Y" functionality. Also used X and Z accelerometers because of how my IMU is oriented.
    I could absolutely tweak with the signed short values to reduce how quickly my mouse moves around on Ubuntu linux, but I'll just leave it as is for now.

--- HW11/firmware/src/system_config/default/system_config.h
+++ HW11/firmware/src/system_config/default/system_config.h
@@ -18,8 +18,8 @@
     definitions for build-time configuration options that are not instantiated
     until used by another MPLAB Harmony module or application.
 
-    Created with MPLAB Harmony Version 2.05
-*******************************************************************************/
+    Created with MPLAB Harmony Version 1.10
+ *******************************************************************************/
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
@@ -43,21 +43,22 @@ INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
 CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
 SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 (INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
-*******************************************************************************/
+ *******************************************************************************/
 // DOM-IGNORE-END
 
 #ifndef _SYSTEM_CONFIG_H
 #define _SYSTEM_CONFIG_H
+
 
 // *****************************************************************************
 // *****************************************************************************
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-/*  This section Includes other configuration headers necessary to completely
-    define this configuration.
-*/
 
+
+/*  This section Includes other configuration headers necessary to completely
+    define this configuration. */
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -67,30 +68,37 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System Service Configuration
 // *****************************************************************************
 // *****************************************************************************
-// *****************************************************************************
-/* Common System Service Configuration Options
-*/
-#define SYS_VERSION_STR           "2.05"
-#define SYS_VERSION               20500
 
-// *****************************************************************************
-/* Clock System Service Configuration Options
-*/
+
+/* Common System Service Configuration Options */
+#define SYS_VERSION_STR             "1.10"
+#define SYS_VERSION                 11000
+
+/* Clock System Service Configuration Options */
 #define SYS_CLK_FREQ                        48000000ul
 #define SYS_CLK_BUS_PERIPHERAL_1            48000000ul
 #define SYS_CLK_UPLL_BEFORE_DIV2_FREQ       96000000ul
 #define SYS_CLK_CONFIG_PRIMARY_XTAL         8000000ul
-#define SYS_CLK_CONFIG_SECONDARY_XTAL       32768ul
-   
-/*** Ports System Service Configuration ***/
+#define SYS_CLK_CONFIG_SECONDARY_XTAL       0ul
 
-/*** Interrupt System Service Configuration ***/
+/* Interrupt System Service Configuration */
 #define SYS_INT                     true
+
+/* Ports System Service Configuration */
+#define SYS_PORT_B_ANSEL            0xac1f
+#define SYS_PORT_B_TRIS             0xefbf
+#define SYS_PORT_B_LAT              0x0
+#define SYS_PORT_B_ODC              0x0
+#define SYS_PORT_B_CNPU             0x0
+#define SYS_PORT_B_CNPD             0x0
+#define SYS_PORT_B_CNEN             0x0
+
 
 // *****************************************************************************
 // *****************************************************************************
@@ -104,38 +112,26 @@ extern "C" {
 // *****************************************************************************
 // *****************************************************************************
 
+
 /*** USB Driver Configuration ***/
 
-
 /* Enables Device Support */
-#define DRV_USBFS_DEVICE_SUPPORT      true
+#define DRV_USBFS_DEVICE_SUPPORT        true
 
-/* Disable Host Support */
-#define DRV_USBFS_HOST_SUPPORT      false
+/* Disable Device Support */
+#define DRV_USBFS_HOST_SUPPORT          false
 
 /* Maximum USB driver instances */
-#define DRV_USBFS_INSTANCES_NUMBER    1
+#define DRV_USBFS_INSTANCES_NUMBER      1
 
 /* Interrupt mode enabled */
-#define DRV_USBFS_INTERRUPT_MODE      true
-
+#define DRV_USBFS_INTERRUPT_MODE        true
 
 /* Number of Endpoints used */
-#define DRV_USBFS_ENDPOINTS_NUMBER    1
-
-
+#define DRV_USBFS_ENDPOINTS_NUMBER      2
 
 
 /*** USB Device Stack Configuration ***/
-
-
-
-
-
-
-
-
-
 
 /* The USB Device Layer will not initialize the USB Driver */
 #define USB_DEVICE_DRIVER_INITIALIZE_EXPLICIT
@@ -144,37 +140,17 @@ extern "C" {
 #define USB_DEVICE_INSTANCES_NUMBER     1
 
 /* EP0 size in bytes */
-#define USB_DEVICE_EP0_BUFFER_SIZE      64
+#define USB_DEVICE_EP0_BUFFER_SIZE      8
 
-/* Enable SOF Events */ 
-#define USB_DEVICE_SOF_EVENT_ENABLE     
-
-
-
-
-
-
-
+/* Enable SOF Events */
+#define USB_DEVICE_SOF_EVENT_ENABLE
 
 /* Maximum instances of HID function driver */
-#define USB_DEVICE_HID_INSTANCES_NUMBER     1
+#define USB_DEVICE_HID_INSTANCES_NUMBER         1
 
-
-
-
-
-
-
-
-
-
-/* HID Transfer Queue Size for both read and
-   write. Applicable to all instances of the
-   function driver */
-#define USB_DEVICE_HID_QUEUE_DEPTH_COMBINED 2
-
-
-
+/* HID Transfer Queue Size for both read and write.
+   Applicable to all instances of the function driver */
+#define USB_DEVICE_HID_QUEUE_DEPTH_COMBINED     2
 
 
 // *****************************************************************************
@@ -182,10 +158,12 @@ extern "C" {
 // Section: Application Configuration
 // *****************************************************************************
 // *****************************************************************************
-/*** Application Defined Pins ***/
 
 
 /*** Application Instance 0 Configuration ***/
+
+/* Macro defines USB internal DMA Buffer criteria*/
+#define APP_MAKE_BUFFER_DMA_READY
 
 //DOM-IGNORE-BEGIN
 #ifdef __cplusplus
@@ -193,7 +171,7 @@ extern "C" {
 #endif
 //DOM-IGNORE-END
 
+
 #endif // _SYSTEM_CONFIG_H
-/*******************************************************************************
- End of File
-*/
+
+/* End of File */
