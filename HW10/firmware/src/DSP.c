43f109b516dc6309b8d1337d062c1eae075aebed
William Spies <spieswl18@u.northwestern.edu>
2018-05-09 21:00:29 -0500

    Added filter functions to a DSP header, need to add weights to FIR filter and add main-line code to leverage those filtering capabilities to tasks.

---
+++ HW10/firmware/src/DSP.c
@@ -0,0 +1,48 @@
+#include <xc.h>
+#include "DSP.h"
+
+void clear_data_array(signed short * data_array, unsigned char length)
+{
+    unsigned char index;
+    
+    for(index = 0; index < length; ++index)
+    {
+        data_array[index] = 0;
+    }
+}
+
+signed short movAvgFilter(signed short * data, unsigned char sampCount)
+{
+    unsigned char index;
+    signed short result = 0;
+    
+    for(index = 0; index < sampCount; ++index)
+    {
+        result += data[index];
+    }
+    
+    return result;
+}
+
+signed short finImpRespFilter(signed short * data, unsigned char sampCount)
+{
+    unsigned char index;
+    signed short weights[8] = {0,0,0,0,0,0,0,0};
+    signed short result = 0;
+    
+    for(index = 0; index < sampCount; ++index)
+    {
+        result += (weights[index] * data[index]);
+    }
+    
+    return result;
+}
+
+signed short infImpRespFilter(signed short * data, signed short priorValue, signed short weightOld, signed short weightNew)
+{
+    signed short result = 0;
+    
+    result = (weightOld * priorValue) + (weightNew * data[0]);
+    
+    return result;
+}
---
+++ HW10/firmware/src/DSP.h
@@ -0,0 +1,10 @@
+#ifndef _DSP_H
+#define _DSP_H
+
+void clear_data_array(signed short * data_array, unsigned char length);
+
+signed short movAvgFilter(signed short * data, unsigned char sampCount);
+signed short finImpRespFilter(signed short * data, unsigned char sampCount);
+signed short infImpRespFilter(signed short * data, signed short priorValue, signed short weightOld, signed short weightNew);
+
+#endif /* _DSP_H */
