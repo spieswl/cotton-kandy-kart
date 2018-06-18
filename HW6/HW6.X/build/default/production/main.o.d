0ce2999c96d0b4e9b73fc3bda77bccc0f22560d0
William Spies <spieswl18@u.northwestern.edu>
2018-06-18 09:37:51 -0500

    Reconfigured old ME433 repository into a dedicated repo (while preserving the commit history) for the aptly-named Cotton Kandy robotic kart.

--- HW6/HW6.X/build/default/production/main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/main.o:  \
-main.c  \
-ST7735.h 
--- HW8/firmware/HW8.X/build/default/production/_ext/1360937237/main.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1360937237/main.o:  \
-../src/main.c  \
- ../../../../../../../../../../opt/microchip/harmony/v2_05/framework/system/common/sys_module.h  \
- ../../../../../../../../../../opt/microchip/harmony/v2_05/framework/system/common/sys_common.h 
